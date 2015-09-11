c86e6195b1905b2cfb163a7bd13ecf1ca12c3498
beatfreaker <radiachints1512@gmail.com>
2015-09-11 23:48:19 +0530

    init

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var gh = require('github-url-to-object');
 var got = require('got');
-var isOnline = require('is-online');
 var param = process.argv.slice(2);
 var userDetails = gh(param[0]);
 var url = 'https://api.github.com/repos/';
@@ -10,7 +9,7 @@ url = url + userDetails.user + '/' + userDetails.repo;
 
 got(url, function (err, data) {
 	if (err) {
-		console.log("error");
+		console.log('error');
 	}
 	var jsonData = JSON.parse(data);
 	if (jsonData.homepage && jsonData.homepage !== '') {
